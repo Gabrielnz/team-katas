ed76da9585204aee78f5b2649c73222586735c75
Julian <julian.sanz@domingoalonsogroup.com>
2020-04-25 18:25:36 +0100

    refactor rename method

--- StringCalculator/StringCalculator/UnitTest1.cs
+++ StringCalculator/StringCalculator/UnitTest1.cs
@@ -15,7 +15,7 @@ namespace StringCalculator {
         public void returns_zero_when_there_are_no_numbers() {
             const string numbers = "";
 
-            var result = calculator.Sum(numbers);
+            var result = calculator.Add(numbers);
 
             result.Should().Be(0);
         }
@@ -24,14 +24,14 @@ namespace StringCalculator {
         public void returns_number_eight_when_the_input_is_eight() {
             const string numbers = "8";
 
-            var result = calculator.Sum(numbers);
+            var result = calculator.Add(numbers);
 
             result.Should().Be(8);
         }
     }
 
     public class Calculator {
-        public int Sum(string numbers) {
+        public int Add(string numbers) {
             if (numbers == "") return 0;
             if (numbers == "8") return 8;
             return default(int);
