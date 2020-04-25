64e1be89396d9411ec71539b53abc565ba6e0648
Luis Eduardo Figuera Dorta <luis.figuera@domingoalonsogroup.com>
2020-04-25 18:12:02 +0100

    green returns_number_eight_when_the_input_is_eight

--- StringCalculator/StringCalculator/UnitTest1.cs
+++ StringCalculator/StringCalculator/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -18,10 +19,21 @@ namespace StringCalculator {
 
             result.Should().Be(0);
         }
+
+        [Test]
+        public void returns_number_eight_when_the_input_is_eight() {
+            const string numbers = "8";
+
+            var result = calculator.Sum(numbers);
+
+            result.Should().Be(8);
+        }
     }
 
     public class Calculator {
         public int Sum(string numbers) {
+            if (numbers == "") return 0;
+            if (numbers == "8") return 8;
             return 0;
         }
     }
